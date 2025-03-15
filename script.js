c827dbff558530a8cd9b9507045baa061e19e909
Khaled Salloum <khaledsalloumm098@gmail.com>
2025-03-15 15:05:39 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById('modifyBtn').addEventListener('click', function() {
+    
+    document.getElementById('content').classList.add('modified');
+  
+    
+    document.getElementById('heading').textContent = 'Updated Heading ✅';
+  });
+  
